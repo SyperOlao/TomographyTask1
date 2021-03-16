9e19859149ef2a17448017fbb5bccfe375b9623a
Syper_olao <olay1234538@gmail.com>
2021-03-16 21:58:39 +0300

    Добавьте файлы проекта.

---
+++ TomographyTask1/src/Parallel.cs
@@ -0,0 +1,14 @@
+﻿namespace TomographyTask1.src
+{
+    class Parallel
+    {
+        double I { get; set; } //Интенсивность 
+        double I0{ get; set; } //Начальная интенсивность 
+        double N { get; set; } //Количество датчиков 
+        double n { get; set; } //Расстояние между соседними датчиками   
+        int k { get; set; } // Номер дачика 
+        int R { get; set; } // Радиус зоны сканирования 
+
+
+    }
+}
---
+++ TomographyTask1/src/ScanLine.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace TomographyTask1.src
+{
+    class ScanLine
+    {
+        public void DrawScanLine(Graphics graphics, Point point1, Point point2, int heighs)
+        {
+            graphics.DrawRectangle(new Pen(Color.Black), point1.X, point1.Y, heighs, heighs);
+            graphics.DrawLine(new Pen(Color.Black), point1, point2); 
+        }
+    }
+}
