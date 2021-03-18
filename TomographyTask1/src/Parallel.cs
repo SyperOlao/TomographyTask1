b2babb627cd969946d847a86e5ccb698b41c703a
Syper_olao <olay1234538@gmail.com>
2021-03-18 22:11:01 +0300

    добавлена перегрузка метода орисовки

--- TomographyTask1/src/Parallel.cs
+++ TomographyTask1/src/Parallel.cs
@@ -1,14 +1,36 @@
-﻿namespace TomographyTask1.src
+﻿using System;
+using System.Drawing;
+
+namespace TomographyTask1.src
 {
     class Parallel
     {
+        private ScanLine scanline = new ScanLine(); 
         double I { get; set; } //Интенсивность 
         double I0{ get; set; } //Начальная интенсивность 
         double N { get; set; } //Количество датчиков 
-        double n { get; set; } //Расстояние между соседними датчиками   
+        int n { get; set; } //Расстояние между соседними датчиками   
         int k { get; set; } // Номер дачика 
         int R { get; set; } // Радиус зоны сканирования 
+        int M { get; set; } // Количество ракурсов 
 
+        public void DrawLines(Graphics graphics, Point point1, Point point2)
+        {
+            double x1 = point1.X;
+            double y1 = point1.X;
+            double x2 = point2.Y;
+            double y2 = point2.Y;
+            for (int i = 0; i < N; i+=n)
+            {
+                scanline.DrawScanLine(graphics, point1, point2, 20);
+                point1.X *= Math.Cos(Math.PI/2 + GetFm(i)) 
+            }
+           
+        }
 
+        private double GetFm(int m)
+        {
+            return n * m / M; //Угол alpha
+        }
     }
 }
--- TomographyTask1/src/ScanLine.cs
+++ TomographyTask1/src/ScanLine.cs
@@ -9,5 +9,11 @@ namespace TomographyTask1.src
             graphics.DrawRectangle(new Pen(Color.Black), point1.X, point1.Y, heighs, heighs);
             graphics.DrawLine(new Pen(Color.Black), point1, point2); 
         }
+
+        public void DrawScanLine(Graphics graphics, double x1, double y1, double x2, double y2,int heighs)
+        {
+            graphics.DrawRectangle(new Pen(Color.Black), (float)x1, (float)y1, heighs, heighs);
+            graphics.DrawLine(new Pen(Color.Black), (float)x1, (float)y1, (float)x2, (float)y2);
+        }
     }
 }
